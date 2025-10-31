5e83dedecdcd6847320b47adf5659aefaf2cdca6
g-99215544-beep <g-99215544@moe-dl.edu.my>
2025-10-31 08:17:16 +0800

    Add firebaseConfig.js with project settings
    Added Firebase configuration details for the project.

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+// PENTING: Isi butiran ini dengan konfigurasi Firebase anda sendiri
+// Anda boleh dapatkan ini dari Firebase Console > Project Settings
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQqc9khxreshF4LJVcRtigVqQNd0g-g3g",
+  authDomain: "sistempengurusan-aset-ict-sksa.firebaseapp.com",
+  projectId: "sistempengurusan-aset-ict-sksa",
+  storageBucket: "sistempengurusan-aset-ict-sksa.firebasestorage.app",
+  messagingSenderId: "891315232628",
+  appId: "1:891315232628:web:ec691478c87cfec0985621",
+  measurementId: "G-QKRJ0G00ZN"
+};
+
+// Sediakan config untuk diimport oleh skrip lain
+export { firebaseConfig };
+
